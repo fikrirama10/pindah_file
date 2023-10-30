0235b3421329306aae4c238aed9860235a9f157d
Fikri Ramadhan <ramadhan@kabayan.id>
2023-10-30 15:43:17 +0700

    commit

--- pindah-file.py
+++ pindah-file.py
@@ -340,6 +340,31 @@ def pindah_file_nib():
         except Exception as e:
             print(f"Kesalahan lain: {e}")
 
+def pindah_file_rencana_konfigurasi():
+    connection = destinantion_connection()
+    cursor = connection.cursor()
+    cursor.execute(" SELECT file_rencana_konfigurasi from lampiran_penyiaran WHERE file_rencana_konfigurasi IS NOT NULL ")
+    records = cursor.fetchall()
+    for row in records:
+        nama_file = row[0]
+        # print(nama_file)
+        # Tentukan path sumber dan path destinasi
+        path_sumber = "/var/www/html/app-simp3-pra-migrasi/uploads/lampiran_file_temp/"+str(nama_file)
+        path_destinasi = "/home/simp3-rebuild/public/storage/file_rencana_konfigurasi_jaringan/"
+        try:
+            with open(path_sumber, "r") as file:
+                # Lakukan operasi pada file
+                shutil.copy(path_sumber, path_destinasi)
+                print("berhasil")
+        except IsADirectoryError as e:
+            print(f"Kesalahan: Ini adalah direktori, bukan file. {e} "+str(nama_file))
+        except FileNotFoundError:
+            print("Kesalahan: File tidak ditemukan.")
+        except PermissionError:
+            print("Kesalahan: Izin akses tidak mencukupi.")
+        except Exception as e:
+            print(f"Kesalahan lain: {e}")
+
 def pindah():
     # using input() to take user input
     print("Pilih Nomor Untuk Pindah File :")
@@ -356,6 +381,7 @@ def pindah():
     print("11. Pindah File Aspek Managemen")
     print("12. Pindah File PKS MUX")
     print("13. Pindah File Lampiran NIB")
+    print("14. Pindah File Konfigurasi Jaringan")
 
 
     num = input('Masukan Nomor: ')
@@ -398,6 +424,9 @@ def pindah():
             pindah_file_pks_mux()
         case '13':
             print("Pindah File NIB")
+            pindah_file_nib()
+        case '14':
+            print("Pindah File Konfigurasi Jaringan")
             pindah_file_nib()
         case _:
             print('Invalid input')
